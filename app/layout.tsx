e2c97314285f8c3b82e07169cd0c4416313e0c70
iam-baivab <baivabbandel@gmail.com>
2024-07-15 01:04:58 +0530

    success auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { dark } from '@clerk/themes';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <ClerkProvider>
+      <ClerkProvider
+      appearance={{
+          baseTheme: dark,
+          
+        layout: {
+          socialButtonsVariant: "iconButton",
+          logoImageUrl: "/icons/yoom-logo.svg",
+        },
+        variables: {
+          colorText: "#fff",
+          colorPrimary: "#0E78F9",
+          colorBackground: "#1C1F2E",
+          colorInputBackground: "#252A41",
+          colorInputText: "#fff",
+        },
+      }}
+      >
         <body className={`${inter.className} bg-dark-2`}>
           {children}
         </body>
